73d5f621330d2b47bcdaf570b19c8c445e92c140
Bruno Brilhante <brunobrilhante53@gmail.com>
2022-06-21 23:01:47 -0300

    Listando todas as Categoras com DTO

--- src/main/java/com/brunobrilhante/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/brunobrilhante/cursospringboot/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.brunobrilhante.cursospringboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +41,10 @@ public class CategoriaService {
 		}
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		}
-		
+		}	
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
