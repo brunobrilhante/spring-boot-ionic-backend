ac54b1e9bc5e89074dec365dba7caad81a58976d
Bruno Brilhante <brunobrilhante53@gmail.com>
2022-06-28 22:34:05 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/brunobrilhante/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/brunobrilhante/cursospringboot/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brunobrilhante.cursospringboot.domain.Categoria;
+import com.brunobrilhante.cursospringboot.dto.CategoriaDTO;
 import com.brunobrilhante.cursospringboot.repositories.CategoriaRepository;
 import com.brunobrilhante.cursospringboot.services.exceptions.DataIntegrityException;
 import com.brunobrilhante.cursospringboot.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
