1bd2c994da4ebd77102e241e772c336b54456ee2
Bruno Brilhante <brunobrilhante53@gmail.com>
2022-07-19 21:04:47 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/brunobrilhante/cursospringboot/dto/ClienteDTO.java
+++ src/main/java/com/brunobrilhante/cursospringboot/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.brunobrilhante.cursospringboot.domain.Cliente;
+import com.brunobrilhante.cursospringboot.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
